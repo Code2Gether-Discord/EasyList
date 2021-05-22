48377379e7e0adb9149c81d47b1cc2b0d48f5ec8
chrisk <christian.khalil200@gmail.com>
2021-05-22 13:12:46 +0200

    Add project files.

---
+++ EasyList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EasyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
