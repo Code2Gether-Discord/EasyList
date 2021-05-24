728c1d5427ec50277b7c3a92a7b82a90a6c9393f
Drag-3 <67492100+Drag-3@users.noreply.github.com>
2021-05-23 20:39:21 -0400

    Create Todo.cs
    Made bare class.

---
+++ EasyList/Todo.cs
@@ -0,0 +1,22 @@
+﻿namespace EasyList
+{
+    public class Todo
+    {
+        public int Id { get; } //Only set on creation
+
+        public string Title { get; } // --
+
+        public bool Completion { get; set; }
+        
+        public Todo(int id, string title)
+        {
+            Id = id;
+            Title = title;
+            Completion = false;
+        }
+
+        
+        public override string ToString() => $"{Id} " + Title + $"\nStatus: {(Completion ? "Complete" : "In Progress")}";
+        
+    }
+}
