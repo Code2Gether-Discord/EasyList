a56713b6eb40baaa6b54687d80e6fac30f829fa6
Drag-3 <67492100+Drag-3@users.noreply.github.com>
2021-05-24 11:22:49 -0400

    Update Todo.cs
    More Constructors because why not.

--- EasyList/Todo.cs
+++ EasyList/Todo.cs
@@ -2,32 +2,72 @@
 
 namespace EasyList
 {
+    public enum TodoPriority
+    {
+        Low,
+        Normal,
+        High
+    }
     public class Todo
     {
-        public int Id { get; } //Only set on creation
-
-        public string Title { get; } // --
-
+        public int Id { get; } 
+        public string Title { get; set; } 
         public bool Completion { get; set; }
+        public TodoPriority Priority { get; set; }
+        public string Description { get; set; }
         
+        public Todo(int id)
+        {
+            Id = id;
+            Title = string.Empty;
+            Description = string.Empty;
+            Completion = false;
+            Priority = TodoPriority.Normal;
+        }
         public Todo(int id, string title)
         {
             Id = id;
             Title = title;
+            Description = string.Empty;
             Completion = false;
+            Priority = TodoPriority.Normal;
         }
-        
-        protected Todo() // add a protected default constructor ?
+        public Todo(int id, string title, string description)
         {
-            //Id = 0;
-            //Title = string.Empty;
-            //Completion = false;
+            Id = id;
+            Title = title;
+            Description = description;
+            Completion = false;
+            Priority = TodoPriority.Normal;
         }
-
-
+        public Todo(int id, string title, TodoPriority priority)
+        {
+            Id = id;
+            Title = title;
+            Description = string.Empty;
+            Completion = false;
+            Priority = priority;
+        }
+        public Todo(int id, string title, string description, TodoPriority priority)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Completion = false;
+            Priority = priority;
+        }
         public override string ToString()
         {
-            var output = new StringBuilder($"{Id} ");
+            var output = new StringBuilder($"{Id}");
+            var priorityChar = Priority switch
+            {
+                TodoPriority.Low => "L",
+                TodoPriority.Normal => "N",
+                TodoPriority.High => "H",
+                _ => string.Empty
+            };
+
+            output.Append($"{priorityChar} ");
             output.AppendLine(Title);
             output.Append($"Status: {(Completion ? "Complete" : "In Progress")}");
             return output.ToString();
