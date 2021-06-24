c7b1817ea4d30f63e1a9310c47541b132031f926
18-F-cali <bloodontimetest@gmail.com>
2021-06-25 01:24:34 +0530

    removed acess modifiers from interfaces

--- EasyList/ITodoRepository.cs
+++ EasyList/ITodoRepository.cs
@@ -4,14 +4,14 @@ using System.Collections.Generic;
 namespace EasyList
 {
 
-    public interface ITodoRepository
+    interface ITodoRepository
     {
         
-        public Todo Add(Todo newTodo);
-        public string Delete(int Id);
-        public Todo Get(int Id);
-        public Todo MarkAsDone(int Id);
-        public Todo Edit(int Id,string? Label = null, string? Description = null, DateTimeOffset? DueDate = null, TodoPriority priority = 0, TodoStatus status = TodoStatus.INPROGRESS);
-        public IEnumerable<Todo> GetAllTask(TodoOrder orderOfList = TodoOrder.DUEDATE);
+        Todo Add(Todo newTodo);
+        string Delete(int Id);
+        Todo Get(int Id);
+        Todo MarkAsDone(int Id);
+        Todo Edit(int Id,string? Label = null, string? Description = null, DateTimeOffset? DueDate = null, TodoPriority priority = 0, TodoStatus status = TodoStatus.INPROGRESS);
+        IEnumerable<Todo> GetAllTask(TodoOrder orderOfList = TodoOrder.DUEDATE);
     }
 }
